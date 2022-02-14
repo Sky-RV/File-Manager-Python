c4a9bfbec92fb7fd4d9c15afcc814d56c4572e16
Sky <90393918+Sky-RV@users.noreply.github.com>
2022-02-14 23:14:01 +0330

    Write File Function

--- File-Manager.py
+++ File-Manager.py
@@ -1,9 +1,41 @@
+import sys
 from colorama import Fore, init, Style
 from os import system
 
 ############################## READ FILE ##############################
 
 ############################## WRITE FILE ##############################
+
+def WriteFile():
+    system('cls')
+
+    writeFile = input(Fore.GREEN + " Enter File's Name : " + Style.RESET_ALL)
+
+    writeFile = writeFile + '.txt'
+
+    myfile = open(writeFile, "a") # append to the end of the file
+
+    myText = input(Fore.GREEN + "\n Enter your text : " + Style.RESET_ALL + " ")
+
+    myText = myText + "\n"
+
+    myfile.write(myText)
+
+    myfile.close()
+
+    print(Fore.LIGHTGREEN_EX + "\n Writed in file successfully.\n" + Style.RESET_ALL)
+
+    print(
+        Fore.RED + ' [1] ' + Fore.WHITE + ' Back to main\n'+
+        Fore.LIGHTBLACK_EX + " Press any key to exit..."
+    )
+
+    answer = input(Fore.GREEN + " >> " + Style.RESET_ALL)
+
+    if answer == '1':
+        main()
+    else:
+        pass
 
 ############################## CREATE FILE ##############################
 
@@ -17,11 +49,11 @@ def CreateFile():
 
     myfile = open(newFile, "w") # create a new file if it doesn't exist
 
-    print(Fore.LIGHTGREEN_EX + " Created file successfully." + Style.RESET_ALL)
+    print(Fore.LIGHTGREEN_EX + "\n Created file successfully.\n" + Style.RESET_ALL)
 
     print(
         Fore.RED + ' [1] ' + Fore.WHITE + ' Write current file\n' +
-        Fore.RED + ' [2] ' + Fore.WHITE + ' back to main\n'+
+        Fore.RED + ' [2] ' + Fore.WHITE + ' Back to main\n'+
         Fore.LIGHTBLACK_EX + " Press any key to exit..."
     )
 
@@ -30,7 +62,7 @@ def CreateFile():
     if answer == '1':
         pass
     elif answer == '2':
-        pass
+        main()
     else:
         pass
 
@@ -46,7 +78,7 @@ def main():
         Fore.RED + ' [1] ' + Fore.WHITE + "Read File\n" +
         Fore.RED + ' [2] ' + Fore.WHITE + "Write File\n" +
         Fore.RED + ' [3] ' + Fore.WHITE + "Create File\n" +
-        Fore.RED + ' [4] ' + Fore.WHITE + "Delete File\n" +
+        Fore.RED + ' [4] ' + Fore.WHITE + "Delete File\n\n" +
         Fore.LIGHTBLACK_EX + " Press any key to exit..."
     )
 
@@ -55,7 +87,7 @@ def main():
     if answer == '1':
         pass
     elif answer == '2':
-        pass
+        WriteFile()
     elif answer == '3':
         CreateFile()
     elif answer == '4':
