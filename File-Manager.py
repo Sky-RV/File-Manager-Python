dd10e7087cea1b82844bde4d02dee16166c4b023
Sky <90393918+Sky-RV@users.noreply.github.com>
2022-02-14 22:56:12 +0330

    Create File Function

--- File-Manager.py
+++ File-Manager.py
@@ -7,6 +7,33 @@ from os import system
 
 ############################## CREATE FILE ##############################
 
+def CreateFile():
+    
+    system('cls')
+
+    newFile = input(Fore.GREEN + " Enter File's Name : " + Style.RESET_ALL)
+
+    newFile = newFile + '.txt'
+
+    myfile = open(newFile, "w") # create a new file if it doesn't exist
+
+    print(Fore.LIGHTGREEN_EX + " Created file successfully." + Style.RESET_ALL)
+
+    print(
+        Fore.RED + ' [1] ' + Fore.WHITE + ' Write current file\n' +
+        Fore.RED + ' [2] ' + Fore.WHITE + ' back to main\n'+
+        Fore.LIGHTBLACK_EX + " Press any key to exit..."
+    )
+
+    answer = input(Fore.GREEN + " >> " + Style.RESET_ALL)
+
+    if answer == '1':
+        pass
+    elif answer == '2':
+        pass
+    else:
+        pass
+
 ############################## DELETE FILE ##############################
 
 ############################## MAIN ##############################
@@ -30,7 +57,7 @@ def main():
     elif answer == '2':
         pass
     elif answer == '3':
-        pass
+        CreateFile()
     elif answer == '4':
         pass
     else:
